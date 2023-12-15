5a44fe66b547a7f82082ba79f9d5f69025745347
huni93 <pkh0699@naver.com>
2023-12-15 13:34:52 +0900

    init

---
+++ src/main/java/ch11/Model2Servlet.java
@@ -0,0 +1,36 @@
+package ch11;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch08.KicMember;
+import ch08.MemberDao;
+
+@WebServlet("/memberinfo")
+public class Model2Servlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		MemberDao md = new MemberDao();
+		KicMember mem = null;
+		try {
+		mem = md.oneMember("222");
+		System.out.println(mem);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		req.setAttribute("mem", mem);
+		RequestDispatcher disp = req.getRequestDispatcher("/chap11_servlet/memberinfo.jsp");
+		disp.forward(req, resp);
+				
+	}
+}
---
+++ src/main/java/ch11/NowServlet.java
@@ -0,0 +1,32 @@
+package ch11;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+//wep.xml 에서 servlet 연결
+public class NowServlet extends HttpServlet {
+
+@Override	
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.setContentType("text/html; charset=utf-8");
+	
+	PrintWriter out = response.getWriter();
+	out.print("<html>");
+	out.print("<head><title>현재시간</title></head>");
+	out.print("<body>");
+	out.print("현재 시간은");
+	out.print(new Date());
+	out.print("입니다.");
+	out.print("</body></html>");
+}
+
+}
+
